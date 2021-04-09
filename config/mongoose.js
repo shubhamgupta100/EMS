fec78a7e273ecf27ba907aa926c7469cb17759b2
Shubham Gupta <sg350348@gmail.com>
2021-04-09 13:26:15 +0530

    signin and sign up form modified

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://shubham:q5dbyxGMI8syIP5p@cluster0.8reo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb+srv://shubham:q5dbyxGMI8syIP5p@cluster0.8reo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/Employees');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
